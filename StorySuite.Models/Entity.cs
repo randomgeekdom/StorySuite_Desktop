19edbc9bb1b5a208b1133ffd9617597dd4c4bb63
Anthony C. Jones <randomgeekdom@gmail.com>
2024-10-20 16:35:32 -0500

    starting domain project

---
+++ StorySuite.Models/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace StorySuite.Models
+{
+    public abstract class Entity
+    {
+        public Guid Guid { get; } = Guid.NewGuid();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+
+            var other = obj as Entity;
+            return this.Guid == other?.Guid;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Guid.GetHashCode();
+        }
+    }
+}
---
+++ StorySuite.Models/StoryProject.cs
@@ -0,0 +1,15 @@
+﻿namespace StorySuite.Models
+{
+    public class StoryProject : Entity
+    {
+        private StoryProject(string name)
+        { this.Name = name; }
+
+        public string Name { get; private set; }
+
+        public static StoryProject TryCreate(string name)
+        {
+            return new StoryProject(name);
+        }
+    }
+}
